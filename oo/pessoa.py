c8a42d90b4b2079acfeaf2234195fa61dbc853e2
ewert <ewerton-mendes@hotmail.com>
2020-03-17 20:45:27 -0300

    Criado atributo complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,22 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35): # Atributos de Instância, nome, idade e filhos
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Brian')
-    print(Pessoa.cumprimentar(p)) # faz o mesmo da linha 10
-    print(id(p))
-    print(p.cumprimentar()) # faz o mesmo da linha 8
-    p.nome = 'Ewerton'
-    print(p.nome)
-    print(p.idade)
+    Brian = Pessoa(nome='Brian')
+    Bernardo = Pessoa(nome='Bernador')
+    Ewerton = Pessoa(Brian, Bernardo, nome='Ewerton')
+    print(Pessoa.cumprimentar(Ewerton)) # faz o mesmo da linha 10
+    print(id(Ewerton))
+    print(Ewerton.cumprimentar()) # faz o mesmo da linha 8
+    print(Ewerton.nome)
+    print(Ewerton.idade)
+    for filhos in Ewerton.filhos:
+        print(filhos.nome)
 
